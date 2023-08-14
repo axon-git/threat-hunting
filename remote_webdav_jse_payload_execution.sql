ca8e76a173a7f62bf74e48bcb6dd753ba8c3dd53
axon-git <105203079+axon-git@users.noreply.github.com>
2023-08-14 15:15:48 +0300

    Update remote_webdav_jse_payload_execution.sql

--- remote_webdav_jse_payload_execution.sql
+++ remote_webdav_jse_payload_execution.sql
@@ -1,5 +1,5 @@
 -- This query detects instances of wscript.exe process creations that involve the execution of a .jse (Encoded Jscript) file from a remote WebDav share
--- This activity was observed by the team in a recent InfoStealer campaign targeting Mexico
+-- This activity was observed by the team in a recent InfoStealer campaign targeting Mexico (August 2023)
 -- The query uses Hunters' EDR Process Creation Unified Scheme
 -- It may be possible to retrieve the file hash and further details from its local copy in the WebDav cache directory at %systemdrive%\windows\ServiceProfiles\LocalService\AppData\Local\Temp\TfsStore
 SELECT DISTINCT
