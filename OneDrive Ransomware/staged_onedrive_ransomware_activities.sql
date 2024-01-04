22d4c1705174a2df54c205fae3f4762f8aa84c43
axon-git <105203079+axon-git@users.noreply.github.com>
2024-01-04 14:48:32 +0200

    Update staged_onedrive_ransomware_activities.sql

--- OneDrive Ransomware/staged_onedrive_ransomware_activities.sql
+++ OneDrive Ransomware/staged_onedrive_ransomware_activities.sql
@@ -11,7 +11,7 @@ WITH COLLECTION_ACTION AS (SELECT TIME_SLICE(EVENT_TIME::TIMESTAMP_NTZ, 5, 'SECO
                           FROM RAW.O365_AUDIT_LOGS
                           WHERE WORKLOAD = 'OneDrive'
                           GROUP BY START_TIME, END_TIME, USER_ID, RAW:UserAgent::VARCHAR, RAW:SourceRelativeUrl::VARCHAR, OBJECT_ID
-                          -- performed actions contains at least one of the content retrieval operations
+                          -- performed actions contain at least one of the content retrieval operations
                           HAVING ARRAY_SIZE(ARRAY_INTERSECTION(ALL_ACTIONS, ARRAY_CONSTRUCT('FileDownloaded', 'FileCopied', 'FileSyncDownloadedFull'))) >= 1),
 -- collection complete stage
 COLLECTION_STAGE AS  (SELECT TIME_SLICE(COLLECTION_ACTION.START_TIME::TIMESTAMP_NTZ, 5, 'MINUTE', 'START') AS START_TIME,
@@ -24,7 +24,8 @@ COLLECTION_STAGE AS  (SELECT TIME_SLICE(COLLECTION_ACTION.START_TIME::TIMESTAMP_
                              SOURCE_IPS
                       FROM COLLECTION_ACTION
                       GROUP BY START_TIME, END_TIME, USER_ID, SOURCE_IPS, USER_AGENT
-                      -- the stage contains at least 60 files involved, and more than 1 folder within a 5 minutes time range
+                      -- the stage contains at least 60 files involved and more than 1 folder within a 5-minute time range
+                      -- NOTE: The collection stage can have up to 600 files in a given 5-minute time range, but in small environments, there may not be 600 files in total.
                       HAVING ARRAY_SIZE(INVOLVED_FILES) >= 60 AND ARRAY_SIZE(INVOLVED_FOLDERS) > 1),
 -- 'encryption' activities per file
 ENCRYPTION_ACTION AS (SELECT TIME_SLICE(EVENT_TIME::TIMESTAMP_NTZ, 5, 'SECOND', 'START') AS START_TIME,
@@ -39,9 +40,9 @@ ENCRYPTION_ACTION AS (SELECT TIME_SLICE(EVENT_TIME::TIMESTAMP_NTZ, 5, 'SECOND', 
                           FROM RAW.O365_AUDIT_LOGS
                           WHERE WORKLOAD = 'OneDrive'
                           GROUP BY START_TIME, END_TIME, USER_ID, RAW:UserAgent::VARCHAR, RAW:SourceRelativeUrl::VARCHAR, OBJECT_ID
-                          -- performed actions contains at least one of the content modification or deletion operations
+                          -- performed actions contain at least one of the content modification or deletion operations
                           HAVING ARRAY_SIZE(ARRAY_INTERSECTION(ALL_ACTIONS, ARRAY_CONSTRUCT('FileModified', 'FileModifiedExtended', 'FileSyncUploadedFull'))) >= 1 OR
-                                 ARRAY_SIZE(ARRAY_INTERSECTION(ALL_ACTIONS, ARRAY_CONSTRUCT('FileDeleted', 'FileRecycled','FileDeletedFirstStageRecycleBin', 'FileDeletedSecondStageRecycleBin'))) >= 1
+                                 ARRAY_SIZE(ARRAY_INTERSECTION(ALL_ACTIONS, ARRAY_CONSTRUCT('FileDeleted', 'FileRecycled', 'FileDeletedFirstStageRecycleBin', 'FileDeletedSecondStageRecycleBin'))) >= 1
                           ),
 -- 'encryption' complete stage
 ENCRYPTION_STAGE AS  (SELECT TIME_SLICE(ENCRYPTION_ACTION.START_TIME::TIMESTAMP_NTZ, 5, 'MINUTE', 'START') AS START_TIME,
@@ -54,7 +55,7 @@ ENCRYPTION_STAGE AS  (SELECT TIME_SLICE(ENCRYPTION_ACTION.START_TIME::TIMESTAMP_
                              SOURCE_IPS
                       FROM ENCRYPTION_ACTION
                       GROUP BY START_TIME, END_TIME, USER_ID, SOURCE_IPS, USER_AGENT
-                      -- the stage contains at least 60 files involved, and more than 1 folder within a 5 minutes time range
+                      -- the stage contains at least 60 files involved and more than 1 folder within a 5-minute time range
                       HAVING ARRAY_SIZE(INVOLVED_FILES) >= 60 AND ARRAY_SIZE(INVOLVED_FOLDERS) > 1
                       )
 SELECT COLLECTION_STAGE.USER_ID                       AS USER_ID,
@@ -72,6 +73,5 @@ INNER JOIN COLLECTION_STAGE ON -- stages have a time difference of up to 7 days
                                COLLECTION_STAGE.END_TIME <= ENCRYPTION_STAGE.END_TIME AND
                                -- the performing user is the same in both stages
                                COLLECTION_STAGE.USER_ID = ENCRYPTION_STAGE.USER_ID AND
-                               -- files correlation between the stages exceeds at 75% match of file between the stages, measure against the 'smaller' stage
-                               ARRAY_SIZE(ARRAY_INTERSECTION(COLLECTION_STAGE.INVOLVED_FILES, ENCRYPTION_STAGE.INVOLVED_FILES))
-                                   >= IFF(ARRAY_SIZE(COLLECTION_STAGE.INVOLVED_FILES) < ARRAY_SIZE(ENCRYPTION_STAGE.INVOLVED_FILES), ARRAY_SIZE(COLLECTION_STAGE.INVOLVED_FILES) * 0.75,  ARRAY_SIZE(ENCRYPTION_STAGE.INVOLVED_FILES) * 0.75)
+                               -- files correlation between the stages exceeds 75% match of file between the stages, comparing the intersected involved files and the 'smaller' stage' involved files
+                               ARRAY_SIZE(ARRAY_INTERSECTION(COLLECTION_STAGE.INVOLVED_FILES, ENCRYPTION_STAGE.INVOLVED_FILES)) >= MIN(ARRAY_SIZE(COLLECTION_STAGE.INVOLVED_FILES), ARRAY_SIZE(ENCRYPTION_STAGE.INVOLVED_FILES)) * 0.75
